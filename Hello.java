dd508136e73bb1531d5b3915ea7516360ef7a153
vinicius cecat <vinicius.cecat@gmail.com>
2025-03-05 22:11:20 -0300

    Adicionar método olamundo

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        olamundo();
+    }
+    public static void olamundo() {
+        System.out.println("Olá Mundo");
     }
 }
