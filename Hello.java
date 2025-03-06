3d09f606eb4873afb8f206215d48fc1345103632
vinicius cecat <vinicius.cecat@gmail.com>
2025-03-05 22:12:59 -0300

    Adicionar método holaquetal

--- Hello.java
+++ Hello.java
@@ -2,8 +2,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         olamundo();
+        holaquetal();
     }
     public static void olamundo() {
         System.out.println("Olá Mundo");
+    }
+    public static void holaquetal() {
+        System.out.println("Hola que tal");
     }
 }
